7301381549cc85dcc35fe00cfdf245136b515cd4
Aidan Ferguson <2966197F@student.gla.ac.uk>
2022-01-18 22:04:17 +0000

    Chapter 4 tests pass

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     # Create a dict which django will use to replace the template variables
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
     # Return the rendered template
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    # Return the rendered template
+    return render(request, 'rango/about.html')
