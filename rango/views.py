7b2264a2cce502ba7d8758e4e21df09d745bd9de
Aidan Ferguson <2966197F@student.gla.ac.uk>
2022-01-25 18:43:38 +0000

    Added forms

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,7 @@
-from unicodedata import category
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     # Query the database for the top 5 categories by likes (desc)
@@ -41,3 +40,17 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    
+    return render(request, 'rango/add_category.html', {'form': form})
